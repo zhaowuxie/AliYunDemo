282fb70f814f356ed2177c0dfb2435ea1fa03f47
zhaowuxie <545691639@qq.com>
2018-09-05 10:57:54 +0800

    init
    213

---
+++ AliYunDemo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AliYunDemo
+//
+//  Created by 赵兵 on 2018/9/5.
+//  Copyright © 2018年 net.canature. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AliYunDemo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AliYunDemo
+//
+//  Created by 赵兵 on 2018/9/5.
+//  Copyright © 2018年 net.canature. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
