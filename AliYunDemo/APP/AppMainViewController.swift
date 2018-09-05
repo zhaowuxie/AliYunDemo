36403eed4bbae52611a5a1eff1f5a36ea8517898
zhaowuxie <545691639@qq.com>
2018-09-05 11:19:26 +0800

    init
    init

---
+++ AliYunDemo/APP/AppMainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AppMainViewController.swift
+//  AliYunDemo
+//
+//  Created by 赵兵 on 2018/9/5.
+//  Copyright © 2018年 net.canature. All rights reserved.
+//
+
+import UIKit
+
+class AppMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
